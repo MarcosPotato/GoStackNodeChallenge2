672d8ddb3d668de9024c161db579201d41e32135
MarcosPotato <marquinhos2725@hotmail.com>
2020-11-09 21:10:34 -0300

    Add transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,18 +1,35 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { RelationCountAttribute } from 'typeorm/query-builder/relation-count/RelationCountAttribute';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository)
+
+  const transactions = await transactionsRepository.find()
+
+  return response.status(200).json(transactions)
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body
+
+  const createTransactionService = new CreateTransactionService()
+
+  const createTransaction = await createTransactionService.execute({
+    title, 
+    value, 
+    type, 
+    category
+  })
+
+  return response.status(200).json(createTransaction)
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,74 @@
 // import AppError from '../errors/AppError';
+import { v4 } from 'uuid'
+import { getCustomRepository } from 'typeorm'
 
-import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError'
+import Transaction from '../models/Transaction'
+
+import CatergoriesRepository from '../repositories/CategoriesRepository'
+import TransactionRepository from '../repositories/TransactionsRepository'
+
+interface NewTransaction{
+  title: string,
+  value: number,
+  type: "income" | "outcome",
+  category: string
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category } :NewTransaction): Promise<Transaction> {
+    
+    const transactionsRepository = getCustomRepository(TransactionRepository)
+    const categoriesRepository = getCustomRepository(CatergoriesRepository)
+
+    if(value <= 0){
+      throw new AppError("You can't add negative values or Zero (nothing)", 400)
+    }
+
+    if(!(type == "income" || type == "outcome")){
+      throw new AppError("Invalid type", 400)
+    }
+
+    const transaction = {
+      id: v4(),
+      title,
+      value,
+      type,
+      category: {},
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
+
+    const exists = await categoriesRepository.findCategory(category)
+    
+    if(exists){
+      transaction.category = exists
+    } else{
+      const newCategory = categoriesRepository.create({
+        id: v4(),
+        title: category, 
+        created_at: new Date(),
+        updated_at: new Date(),
+      })
+
+      await categoriesRepository.save(newCategory)
+
+      transaction.category = newCategory
+    }
+
+    await transactionsRepository.save(transaction)
+
+    const newTransaction: Transaction = {
+      id: transaction.id,
+      title: transaction.title,
+      value: transaction.value,
+      type: transaction.type,
+      category_id: transaction.category.id,
+      created_at: transaction.created_at,
+      updated_at: transaction.updated_at
+    }
+
+    return newTransaction
   }
 }
 
